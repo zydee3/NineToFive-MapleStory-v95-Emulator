4516aa1883f871868f5e020a0866fda5f081f491
Vince Zydea <dev.vincent@yahoo.com>
2020-06-28 18:56:39 -0700

    1. Added SkillProperties<Enum> to Constants.cs 2. Created ServerTests<Module> to test things like Wz / other future things. 3. First Implementation of Skills. I had to store all Values as a string because some Values were stored as both a WzIntProperty or WzStringProperty depending on the skill so I wasn't able to store the string in an integer variable sometimes. These strings are stored in an array where their index matches directly with Constants.SkillProperties<Enum>. 4. First Implementation of WzProvider. There is a function called EvaluateProperty(string, int?, int?). From what I have seen, there are two variables so far in the wz files, U and X. We can pass in the property, which is stored as a string, to calculate the value. If no calculations needed, just return the int value of the property.

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -21,4 +21,14 @@
     public static class Job {
         public static bool IsExtendedSpJob(int jobId) => !(jobId / 1000 != 3 && jobId / 100 != 22 && jobId != 2001);
     }
+    
+    public enum SkillProperties {
+        acc, asrR, attackCount, bulletCount, cooltime, 
+        cr, criticaldamageMax, criticaldamageMin, damage, 
+        damR, dot, dotInterval, dotTime, emdd, epad, epdd, 
+        er, eva, expR, hpCon, ignoreMobpdpR, jump, mad, 
+        mastery, maxLevel, mdd, mhpR, mobCount, morph, mp, 
+        mpCon, pad, padX, pdd, pddR, prop, range, speed, 
+        subProp, subTime, t, terR, time, u, v, w, x, y, z
+    }
 }
