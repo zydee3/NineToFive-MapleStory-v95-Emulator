12adf72c9bee2161aee754ec847ebc1d800b7518
izarooni <coalasonly@gmail.com>
2020-07-06 23:12:43 -0600

    Removed chat restrictions

--- Ezorsia/dllmain.cpp
+++ Ezorsia/dllmain.cpp
@@ -18,6 +18,13 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		std::cout << "::GetModuleFileNameW hooked" << std::endl;
 		Hooker::NMCO_InitializeProxy();
 		std::cout << "NMCO library loaded" << std::endl;
+
+		// remove chat restrictions
+		Memory::WriteByte(0x004AA5B4, 0xEB);
+		char update[] = {0xe9, 0xbf, 0, 0, 0, 0x90};
+		memcpy((void*) 0x004AA707, &update, sizeof(update));
+		Memory::WriteByte(0x004AA7EF, 0xEB);
+		Memory::FillBytes(0x004290C3, 0x90, 2);
 		break;
 	}
 	default: break;
