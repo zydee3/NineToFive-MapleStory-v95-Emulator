0a1b813198f1388fd2a368333bea6b66cbf50971
Vince Zydea <dev.zydea@gmail.com>
2020-08-12 04:39:18 -0700

    Adding job ids (enum)

---
+++ Common/Constants/Job.cs
@@ -0,0 +1,133 @@
+﻿namespace NineToFive.Constants {
+    public enum Job : int {
+        Beginner = 0,
+        Noblesse = 1000,
+        Legend = 2000,
+        Citizen = 3000,
+
+        Warrior = 100,
+        Magician = 200,
+        Bowman = 300,
+        Thief = 400,
+        Pirate = 500,
+        
+        Fighter = 110,
+        Crusader = 111,
+        Hero = 112,
+        
+        Page = 120,
+        WhiteKnight = 121,
+        Paladin = 122,
+        
+        Spearman = 130,
+        DragonKnight = 131,
+        DarkKnight = 132,
+
+        FirePoisonWizard = 210,
+        FirePoisonMage = 211,
+        FirePoisonArchmage = 212,
+        
+        IceLightningWizard = 220,
+        IceLightningMage = 221,
+        IceLightningArchmage = 222,
+        
+        Cleric = 230,
+        Priest = 231,
+        Bishop = 232,
+
+        Hunter = 310,
+        Ranger = 311,
+        Bowmaster = 312,
+        
+        Crossbowman = 320,
+        Sniper = 321,
+        Marksman = 322,
+
+        Assassin = 410,
+        Hermit = 411,
+        Nightlord = 412,
+        
+        Bandit = 420,
+        ChiefBandit = 421,
+        Shadower = 422,
+        
+        BladeRecruit = 430,
+        BladeAcolyte = 431,
+        BladeSpecialist = 432,
+        BladeLord = 433,
+        BladeMaster = 434,
+
+        Brawler = 510,
+        Marauder = 511,
+        Buccaneer = 512,
+        
+        Gunslinger = 520,
+        Outlaw = 521,
+        Corsair = 522,
+        
+        Manager = 800,
+        GameMaster = 900,
+        SuperGameMaster = 910,
+        
+        DawnWarrior1 = 1100,
+        DawnWarrior2 = 1110,
+        DawnWarrior3 = 1111,
+        DawnWarrior4 = 1112,
+        
+        BlazeWizard1 = 1200,
+        BlazeWizard2 = 1210,
+        BlazeWizard3 = 1211,
+        BlazeWizard4 = 1212,
+        
+        WindArcher1 = 1300,
+        WindArcher2 = 1310,
+        WindArcher3 = 1311,
+        WindArcher4 = 1312,
+        
+        NightWalker1 = 1400,
+        NightWalker2 = 1410,
+        NightWalker3 = 1411,
+        NightWalker4 = 1412,
+        
+        ThunderBreaker1 = 1500,
+        ThunderBreaker2 = 1510,
+        ThunderBreaker3 = 1511,
+        ThunderBreaker4 = 1512,
+        
+        Evan = 2001,
+        Aran1 = 2100,
+        Aran2 = 2110,
+        Aran3 = 2111,
+        Aran4 = 2112,
+        Evan1 = 2200,
+        Evan2 = 2210,
+        Evan3 = 2211,
+        Evan4 = 2212,
+        Evan5 = 2213,
+        Evan6 = 2214,
+        Evan7 = 2215,
+        Evan8 = 2216,
+        Evan9 = 2217,
+        Evan10 = 2218,
+        
+        DemonAvenger1 = 3101,
+        DemonAvenger2 = 3120,
+        DemonAvenger3 = 3121,
+        DemonAvenger4 = 3122,
+        
+        BattleMage1 = 3200,
+        BattleMage2 = 3210,
+        BattleMage3 = 3211,
+        BattleMage4 = 3212,
+        
+        WildHunter1 = 3300,
+        WildHunter2 = 3310,
+        WildHunter3 = 3311,
+        WildHunter4 = 3312,
+        
+        Mechanic1 = 3500,
+        Mechanic2 = 3510,
+        Mechanic3 = 3511,
+        Mechanic4 = 3512,
+    }
+}
