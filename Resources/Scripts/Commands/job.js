88b04fe610af91571015ab20e4eef0f2fad87e3a
izarooni <coalasonly@gmail.com>
2020-08-16 00:18:50 -0600

    Added 'job' command

---
+++ Resources/Scripts/Commands/job.js
@@ -0,0 +1,13 @@
+async function run() {
+    let user = ctx.User;
+    if (ctx.Args.Length != 1) {
+        return user.SendMessage("[command] !job <new job>");
+    }
+    let n = parseInt(ctx.Args[0]);
+    if (isNaN(n)) {
+        return user.SendMessage(`'${ctx.Args[0]}' is not a valid number.`);
+    }
+    user.CharacterStat.Job = ctx.ArgAsInt(0);
+    user.CharacterStat.SendUpdate(user, 0x20);
+    user.SendMessage(`Job is now ${user.CharacterStat.Job}`);
+}
