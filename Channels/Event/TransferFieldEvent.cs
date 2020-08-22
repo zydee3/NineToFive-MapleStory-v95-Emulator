b2e76560d045faebb8b339dbcc5fee9d8f615aa5
izarooni <coalasonly@gmail.com>
2020-08-21 21:01:49 -0600

    Fixed TransferFieldEvent incorrect dsetination portal

--- Channels/Event/TransferFieldEvent.cs
+++ Channels/Event/TransferFieldEvent.cs
@@ -2,18 +2,15 @@
 using System.Linq;
 using System.Numerics;
 using log4net;
-using NineToFive.Constants;
-using NineToFive.Game;
-using NineToFive.Game.Entity.Meta;
 using NineToFive.Net;
-using NineToFive.Wz;
+using NineToFive.Packets;
 
 namespace NineToFive.Event {
     public class TransferFieldEvent : PacketEvent {
         private static readonly ILog Log = LogManager.GetLogger(typeof(TransferFieldEvent));
         private int _targetField;
         private string _portal;
-        private Vector2 _location = Vector2.Zero;
+        private Vector2 _location;
         public TransferFieldEvent(Client client) : base(client) { }
 
         public override bool OnProcess(Packet p) {
@@ -30,53 +27,45 @@ namespace NineToFive.Event {
                     _targetField = Client.User.Field.ReturnMap;
                     return true;
                 }
-                
+
                 _location = new Vector2(p.ReadShort(), p.ReadShort());
             }
 
             p.ReadByte(); // 0
-            byte a3 =p.ReadByte();
+            byte a3 = p.ReadByte();
             byte a4 = p.ReadByte();
-
-            Log.Info($"{_targetField}, '{_portal}', {a3}, {a4}");
-            return true; 
+            return true;
         }
 
         public override void OnHandle() {
             var user = Client.User;
-            
-            bool targetPortalExists = !_portal.Equals("");
-            Portal portal = null;
+            if (user.IsDebugging) {
+                user.SendMessage($"From [{user.Field.Id}, {user.CharacterStat.Portal}, {user.Location}], To [{_targetField}, {_portal}, {_location}]");
+            }
 
-            if(targetPortalExists){
-                try {
-                    portal = user.Field.Portals.First(p => p.Name.Equals(_portal));
-                } catch (Exception exception) {
-                    Log.Warn($"Failed to find portal '{_portal}' in field {user.Field.Id}");
-                    return;
+            if (user.CharacterStat.HP < 1) {
+                // user revive
+                user.CharacterStat.HP = Math.Max(1, user.CharacterStat.MaxHP / 10);
+                user.SetField(user.Field.ReturnMap, null, false);
+                return;
+            }
+
+            var portal = user.Field.Portals.FirstOrDefault(p => p.Name.Equals(_portal));
+            if (portal == null) {
+                user.Client.Session.Write(FieldPackets.GetTransferFieldRequestIgnored(4));
+                return;
+            }
+
+            var dest = Client.Channel.GetField(portal.TargetMap);
+            var destPortal = dest.Portals.FirstOrDefault(p => p.Name.Equals(portal.TargetPortalName));
+            if (destPortal == null) {
+                destPortal = dest.Portals.FirstOrDefault();
+                if (destPortal == null) {
+                    Log.Warn($"No portal found for field {dest.Id}");
                 }
             }
 
-            if (_location != Vector2.Zero) user.Location = _location;
-            if (_targetField == 999999999 || _targetField == -1) _targetField = portal.TargetMap;
-            
-            Field field;
-            try {
-                field = Client.Channel.GetField(_targetField); // throws map not found and cancels the whole event
-            } catch (Exception ignore) {
-                field = Client.Channel.GetField((user.Field.Id/1000000)*1000000); // 103000100 -> 103000000 or 100000100 -> 100000000 nearest town
-            }
-            
-            portal = (portal == null ? 
-                field.Portals[0] : 
-                field.Portals.FirstOrDefault(p => p.TargetPortalName.Equals(portal.Name)));
-
-            user.SetField(field.Id, portal);
-
-            if (!targetPortalExists && user.CharacterStat.HP == 0 && field.Town) {
-                user.CharacterStat.HP = 50;
-                user.CharacterStat.SendUpdate(user, (uint)UserAbility.HP);
-            }
+            user.SetField(portal.TargetMap, destPortal, false);
         }
     }
 }
