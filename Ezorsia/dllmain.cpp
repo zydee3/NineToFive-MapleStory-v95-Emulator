37b8ea0e5dbdac1be5a3bc6c0d4e674575d09157
izarooni <coalasonly@gmail.com>
2020-08-29 17:08:55 -0600

    Fix crash when upping skills

--- Ezorsia/dllmain.cpp
+++ Ezorsia/dllmain.cpp
@@ -22,9 +22,12 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		Discord::StartThread();
 
 		// remove chat restrictions
-		Memory::WriteByte(0x004AA5B4, 0xEB);
+		// // SP_914_YOU_ARE_CURRENTLY_BLOCKED_FROM_CHATTING
+		Memory::WriteByte(0x004AA5B4, 0xEB); 
+		// SP_912_REPEATING_THE_SAME_LINE_OVER_AND_OVER_R_NCAN_NEGATIVELY_AFFECT_OTHER_USERS
 		char update[] = {0xe9, 0xbf, 0, 0, 0, 0x90};
 		memcpy((void*) 0x004AA707, &update, sizeof(update));
+		// SP_913_TOO_MUCH_CHATTING_CAN_DISRUPT_R_NOTHER_PLAYERS_ABILITY_TO_PLAY_THE_GAME
 		Memory::WriteByte(0x004AA7EF, 0xEB);
 		Memory::FillBytes(0x004290C3, 0x90, 2);
 		
@@ -39,9 +42,10 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		// CUserLocal__TryDoingBodyAttack CWvsContext__IsUserGM
 		Memory::WriteByte(0x0093075F, 0xEB);
 		
-		// CUISkill::OnSkillLevelUpButton
-		Memory::WriteByte(0x0084D817, 90);
-		Memory::WriteByte(0x0084D817 + 1, 0xEB);
+		// CUISkill::OnSkillLevelUpButton CWvsContext::IsAdminAccount
+		// 0F 85 D0 06 00 00 -> E9 D1 06 00 00 90
+		update[] = {0xE9, 0xD1, 0x06, 0 0, 0x90};
+		memcpy((void*)0x0084D817, &update, sizeof(update));
 		break;
 	}
 	default: break;
